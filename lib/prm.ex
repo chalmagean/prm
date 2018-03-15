56976920cdb4140b799bd13abdf0472e4b223383
Cezar Halmagean <cezar@mixandgo.com>
2018-03-15 11:29:15 +0200

    Blank slate

--- lib/prm.ex
+++ lib/prm.ex
@@ -1,18 +1,2 @@
 defmodule PRM do
-  @moduledoc """
-  Documentation for PRM.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> PRM.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
